cc977b91a436bee19b9bf0d0489494e9f165ab42
phelmig <phelmig@insidersknowledge.com>
2015-05-19 17:58:17 +0200

    Updated: Localization, Initialization Process, added cookieless authentication, German localization

--- secdash/readme.txt
+++ secdash/readme.txt
@@ -1,23 +1,57 @@
 === SECDASH ===
-Contributors: Patrick Helmig, Nils Hesse
-Plugin URI: http://secdash.com
+Contributors: phelmig
+Plugin URI: https://secdash.com
 Tags: secdash, monitoring, security, updates, auto-update
 Requires at least: 3.0.0
-Tested up to: 4.2
-Stable tag: 0.9.4
+Tested up to: 4.2.2
+Stable tag: 1.0
+License: GPLv2
+License URI: https://raw.githubusercontent.com/secdash/secdash_wp_plugin/master/LICENSE
 
-SECDASH allows website owners to monitor the security of their Webserver, WordPress installation and Plugins in realtime. As soon as a security issue becomes known we notify the website's owner.
+SECDASH allows website owners to monitor the security of their Webserver, WordPress installation and Plugins in almost real time. As soon as a security issue becomes known we notify the website's owner.
 
 == Description == 
 
+NOTE: For WordPress users SECDASH is free for up to three websites.
+
+SECDASH is a cloud-based website monitoring platform that empowers organizations with multiple websites and different Content-Management-Systems to always know if they are online, up-to-date and secure.
+
+We provide organizations the SECDASH plug-in that they install on their website/s. The plug-in continuously sends us data of the website’s software assets (Content-Management-System (CMS), the installed modules, and the webserver). The data includes information such as names, version numbers, etc. At the other end we crawl the web and aggregate information that affects these website components such as available updates and security vulnerabilities.
+
+SECDASH then matches the information and figures out if all parts of the website are okay. If not, SECDASH notifies the right person that is responsible for the affected website and component. In addition, we provide a dashboard that shows and visualizes all the information about the websites, its modules and the webservers.
+
+Try it out here: [secdash.com](https://www.secdash.com/)
+
+== Installation ==
+
+1. Upload the `secdash` folder to `/wp-content/plugins/`
+1. Activate the plugin through the 'Plugins' menu in WordPress
+3. Initialize SECDASH through the item 'SECDAS' under the 'Options' menu
+
 == Changelog ==
 
-0.9.4 Increased backwards compatibility. SECDASH now works even with WP3.0 (we don't recommend using this though ^^)
+= 1.0 =
+*   Major UI improvements
+*   Localization (currently available in German und Swedish)
+*   Improved initialization process
+*   Optional handshake process when cookies are not available
+*   First final release
 
-0.9.3 (re-)include wp-includes/version.php to make sure we get the right WP Version
+= 0.9.5 =
+*   The manual activation key is now shown in a text area to make it more accessible
+*   Instructions / Error messages and UI improvements
 
-0.9.2 Force HTTPS
+= 0.9.4 =
+*   Increased backwards compatibility. SECDASH now works even with WP3.0 (we don't recommend using this though ^^)
 
-0.9.1 Bugfix for older PHP Versions
+= 0.9.3 =
+*   (re-)include wp-includes/version.php to make sure we get the right WP Version
 
-0.9 Initial Release (Beta)
+= 0.9.2 =
+*   Force HTTPS
+
+= 0.9.1 =
+*    Bugfix for older PHP Versions
+
+= 0.9 =
+*   Initial Public Release (Beta)
--- secdash/secdash_options.php
+++ secdash/secdash_options.php
@@ -1,9 +1,10 @@
 <?php
 class SecdashOptions {
+    private $secdash_successful_initialized = 'secdash_successful_initialized';
     private $secdash_shared_secret_name = 'secdash_shared_secret';
 
     public function registerOptionsMenu() {
-        add_options_page( __( 'SECDASH Options', 'secdash' ), __( 'SECDASH', 'secdash' ), 'manage_options', 'secdash-options-menu-identifier', array($this, 'optionsPage'));
+        add_options_page( __( 'Initialize SECDASH', 'secdash' ), __( 'SECDASH', 'secdash' ), 'manage_options', 'secdash-options-menu-identifier', array($this, 'optionsPage'));
 
         add_action('admin_init', array($this, 'registerOptions'));
     }
@@ -16,24 +17,31 @@ class SecdashOptions {
         global $wp_settings_errors;
 
         if (!current_user_can('manage_options'))  {
-            wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+            wp_die( __( 'You do not have sufficient permissions to access this page.', 'secdash' ) );
         }
 
         $wp_settings_errors = get_transient('settings_errors');
     ?>
         <div class="wrap">
-        <h2><?php _e( 'SECDASH Options', 'secdash' ); ?></h2>
-        <?php settings_errors(); set_transient('settings_errors', array()); ?>
-        <form method="post" action="<?php echo admin_url('admin.php'); ?>">
+        <h2><?php _e( 'Initialize SECDASH', 'secdash' ); ?></h2>
 
+            <p>
+            <h3><?php _e('What happens here?','secdash')?></h3>
+                <?php _e('In order to monitor your website SECDASH is required to be able to retrieve the currently installed WordPress- and plugin versions. The initialization process is required to connect this WordPress website to your SECDASH profile and to generate a shared key that is used to retrieve the version information from this website in a secure manner. ','secdash')?>
+            </p>
+            <p>
+                <?php _e('Please note that the initialization is not possible while WordPress is in maintenance mode or while the index.php is not reachable.','secdash')?>
+            </p>
+
+        <form method="post" action="<?php echo admin_url('admin.php'); ?>">
         <input type="hidden" name="action" value="secdash_update_shared_secret" />
 
-        <h4><?php _e( 'For (re-)registering with SECDASH please enter your licence key:', 'secdash' ); ?></h4>
+        <p style="font-weight: bold;"><?php _e( 'To initialize SECDASH please enter your license key (for security reasons this key will NOT be stored in your WordPress database). <br/>The SECDASH license key is available on <a href="https://secdash.com/board" title="SECDASH Homepage">SECDASH.com</a>:', 'secdash' ); ?></p>
     <?php
-        if(get_option($this->secdash_shared_secret_name)) {
+        if(get_option($this->secdash_successful_initialized)) {
     ?>
         <div class='updated settings-error'>
-            <?php _e( 'This plugin is already registered with SECDASH. Submitting this form will result in updating the registration.', 'secdash' ); ?>
+            <?php _e( 'SECDASH has been successfully activated. Re-enter your license key to repeat the initialization (e.g. after the location of this website changed).', 'secdash' ); ?>
         </div>
     <?php
         }
@@ -44,8 +52,9 @@ class SecdashOptions {
             <td><input type="text" name="secdash_license_key" value="" /></td>
             </tr>
         </table>
-
-        <input type="submit" name="submit" id="submit" class="button button-primary" value="<?php _e( 'Save Changes', 'secdash' ); ?>"  />
+            <?php wp_nonce_field('secdash_update_or_init','_wp_secdash_update_or_init'); ?>
+        <?php settings_errors(); set_transient('settings_errors', array()); ?>
+        <input type="submit" name="submit" id="submit" class="button button-primary" value="<?php _e( 'Initialize SECDASH', 'secdash' ); ?>"  />
 
         </form>
         </div>
